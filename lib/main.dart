500b978d47b3efc5feae01f3373e6de5a5ea3b02
davinob <davinob@gmail.com>
2021-08-04 21:31:45 +0300

    changes after release

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-final InAppLocalhostServer localhostServer = new InAppLocalhostServer();
-
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await localhostServer.start();
   runApp(new MyApp());
 }
 
@@ -26,18 +23,14 @@ class _MyAppState extends State<MyApp> {
         home: Scaffold(
       body: WillPopScope(
         onWillPop: () async {
-          webViewController!.loadUrl(
-              urlRequest: URLRequest(
-                  url: Uri.parse(
-                      "http://localhost:8080/assets/html/index.html")));
+          webViewController!.loadFile(assetFilePath: "assets/html/index.html");
+
           return false;
         },
         child: SafeArea(
           child: InAppWebView(
             onWebViewCreated: (controller) => webViewController = controller,
-            initialUrlRequest: URLRequest(
-                url: Uri.parse(
-                    "http://localhost:8080/assets/html/indexIntro.html")),
+            initialFile: "assets/html/indexIntro.html",
             initialOptions: InAppWebViewGroupOptions(
                 crossPlatform: InAppWebViewOptions(
                   supportZoom: false,
